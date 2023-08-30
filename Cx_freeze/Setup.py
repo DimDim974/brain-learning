6b518a8e42ad1f2aade5e1e2c965fd5443096ed2
SAUTRON Dimitri <dimitri.sautron@scalian.com>
2023-08-30 13:37:16 +0200

    TECH

--- Cx_freeze/Setup.py
+++ Cx_freeze/Setup.py
@@ -3,4 +3,5 @@ from cx_Freeze import setup, Executable
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
-setup(executables = [Executable("main.py", base=base,icon="icon.ico")])
+
+setup(executables=[Executable("main.py", base=base, icon="icon.ico")])
